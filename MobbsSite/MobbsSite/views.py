4528f786426c85fe5d806775f1f85cbf34bb5b5d
Riley <riley@caltech.edu>
2022-07-11 17:02:23 -0700

    Makes homepage with ability to link to posts

--- MobbsSite/MobbsSite/views.py
+++ MobbsSite/MobbsSite/views.py
@@ -3,12 +3,23 @@ Renders HTML web pages
 """
 
 from django.http import HttpResponse
+from django.template.loader import render_to_string
+from posts.models import Post
 
 
 def home(request):
     """Take in a request and return HTML as a response"""
-    name = 'Riley'
-    HTML_STRING = f"""
-    <h1>Hello {name}!</h1>
-    """
+    post_obj = Post.objects.get(id=1)
+    post_qs = Post.objects.all()
+
+    context = {
+        "objectlist": post_qs,
+        "title": post_obj.title,
+        "content": post_obj.content,
+        "likes": post_obj.likes
+    }
+    H_STRING = render_to_string("home.html",
+    context = context)
+
+    HTML_STRING = H_STRING
     return HttpResponse(HTML_STRING)
--- MobbsSite/posts/models.py
+++ MobbsSite/posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.TextField()
+    content = models.TextField()
+    likes = 0
