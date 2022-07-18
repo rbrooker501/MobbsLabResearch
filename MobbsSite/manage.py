fc1a7d5472ac74ca11fbdeed98e5625eaa20dcbd
Riley <riley@caltech.edu>
2022-07-18 10:41:37 -0700

    Reverts to pre-prep

--- MobbsSite/manage.py
+++ MobbsSite/manage.py
@@ -2,17 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import dotenv
-import pathlib
 
 
 def main():
     """Run administrative tasks."""
-    DOT_ENV_PATH = pathlib.Path() / '.env'
-    if DOT_ENV_PATH.exists():
-        dotenv.load_dotenv(str(DOT_ENV_PATH))
-    else:
-        print("No .ev found, be sure to make it!")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MobbsSite.settings')
     try:
         from django.core.management import execute_from_command_line
