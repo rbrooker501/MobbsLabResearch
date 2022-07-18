a603f8eb902acc1670cc55e8d47855c798e251c5
ry7xsfa <105337100+ry7xsfa@users.noreply.github.com>
2022-07-18 10:50:33 -0700

    Delete MobbsSite directory

--- MobbsSite/posts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Post(models.Model):
-    title = models.TextField()
-    content = models.TextField()
-    likes = models.IntegerField()
