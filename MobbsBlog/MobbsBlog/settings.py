0f795d815ff10e26db8777603b5ae3e60c9ee36d
Riley <riley@caltech.edu>
2022-07-20 15:41:12 -0700

    Adds user authentication

--- MobbsBlog/MobbsBlog/settings.py
+++ MobbsBlog/MobbsBlog/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Posts'
+    'Posts',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC-7'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
@@ -119,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- MobbsBlog/Posts/urls.py
+++ MobbsBlog/Posts/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-#from . import views
 from .views import HomeView, PostDetailView, PostCreateView, PostEditView, PostDeleteView
 
 urlpatterns = [
