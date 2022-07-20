c03ad4851ae0066d5e23d2352b1e4094507b615f
Riley <riley@caltech.edu>
2022-07-20 12:26:27 -0700

    Adds post deletion

--- MobbsBlog/Posts/urls.py
+++ MobbsBlog/Posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, PostDetailView, PostCreateView, PostEditView
+from .views import HomeView, PostDetailView, PostCreateView, PostEditView, PostDeleteView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('post/create/', PostCreateView.as_view(), name='post-create'),
     path('post/edit/<int:pk>', PostEditView.as_view(), name='post-edit'),
+    path('path/<int:pk>/remove', PostDeleteView.as_view(), name='post-delete')
 ]
--- MobbsBlog/Posts/views.py
+++ MobbsBlog/Posts/views.py
@@ -1,11 +1,14 @@
+from ast import Del
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-id']
 
 class PostDetailView(DetailView):
     model = Post
@@ -14,9 +17,14 @@ class PostDetailView(DetailView):
 class PostCreateView(CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'create.html'
+    template_name = 'post_create.html'
 
 class PostEditView(UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'post_edit.html'
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
