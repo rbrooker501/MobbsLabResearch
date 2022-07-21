13e49ff2288b7cfe208c69fc18e9cbabbaa26c28
Riley <riley@caltech.edu>
2022-07-21 10:45:15 -0700

    Adds fields to registration form

--- MobbsBlog/Posts/views.py
+++ MobbsBlog/Posts/views.py
@@ -8,7 +8,13 @@ from django.http import HttpResponseRedirect
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
     return HttpResponseRedirect(reverse('post-detail', args=[str(pk)]))
 
 
@@ -22,8 +28,14 @@ class PostDetailView(DetailView):
     template_name = 'post_detail.html'
     def get_context_data(self, *args, **kwargs):
         context = super(PostDetailView, self).get_context_data(*args, **kwargs)
-        total_likes = (get_object_or_404(Post, id=self.kwargs['pk'])).total_likes()
+        post = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = post.total_likes()
+        liked = False
+
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
         context['total_likes'] = total_likes
+        context['liked'] = liked
         return context
 
 class PostCreateView(CreateView):
