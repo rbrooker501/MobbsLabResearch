ab24dfc4bc3bbc30bee67669e359a4a4dbec9337
Riley <riley@caltech.edu>
2022-07-21 13:20:53 -0700

    Adds change password form

--- MobbsBlog/accounts/forms.py
+++ MobbsBlog/accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User 
 from django import forms
 
@@ -26,3 +26,12 @@ class EditProfileForm(UserChangeForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password')
+
+class ChangePasswordForm(PasswordChangeForm):
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password'}))
+    new_password1 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password'}))
+    new_password2 = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control', 'type':'password'}))
+
+    class Meta:
+        model = User
+        fields = ('old_password', 'new_password1', 'new_password2')
--- MobbsBlog/accounts/urls.py
+++ MobbsBlog/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView
+from .views import UserRegisterView, UserEditView, ChangePasswordView, ChangedPasswordView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit-profile'),
+    path('password/', ChangePasswordView.as_view(template_name='registration/change-password.html')),
+    path('password_changed/', ChangedPasswordView, name='password-changed')
 ]
