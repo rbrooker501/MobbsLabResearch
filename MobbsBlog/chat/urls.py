64759f2941fe0c568bfbed10bcbe13bf971d6c3c
Riley <riley@caltech.edu>
2022-07-25 15:50:14 -0700

    Adds to test views for chat pages

--- MobbsBlog/chat/urls.py
+++ MobbsBlog/chat/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import roomsView
+from .views import roomsView, chatView
 
 urlpatterns = [
-    path('', roomsView, name='room-select')
+    path('', roomsView, name='room-select'),
+    path('<slug:slug>/', chatView, name='chat-room')
 ]
